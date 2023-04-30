0547a5200e239a9ab64d56da51dd97691b65edf5
phamann <patrick@fastly.com>
2023-04-30 10:29:45 +0100

    WIP: test

--- hosts/yoda/home.nix
+++ hosts/yoda/home.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }: {
-  home.username = "phamann";
-  home.homeDirectory =
-    "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/phamann";
-  home.stateVersion = "22.11";
-  programs.home-manager.enable = true;
-
   imports = [
     ../../modules/default.nix
   ];
-  /* config.modules = {
-    zsh.enable = true;
-    packages.enable = true;
-  }; */
+  config =
+    home.username = "phamann";
+    home.homeDirectory =
+        "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/phamann";
+    home.stateVersion = "22.11";
+    programs.home-manager.enable = true;
+
+    modules = {
+        zsh.enable = true;
+        packages.enable = true;
+    };
+  };
 }
