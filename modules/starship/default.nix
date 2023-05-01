dd6b40155e079233a89d6474fe6efb5d587f2fdf
phamann <patrick@fastly.com>
2023-05-01 19:19:36 +0100

    update starship

--- modules/starship/default.nix
+++ modules/starship/default.nix
@@ -7,6 +7,7 @@ in
   config = mkIf cfg.enable {
     programs.starship = {
       enable = true;
+      enableZshIntegration = true;
       settings = {
         format = lib.concatStrings [
           "$username"
@@ -32,6 +33,9 @@ in
           "$shell"
           "$character"
         ];
+        hostname = {
+          ssh_symbol = "";
+        };
         username = {
           show_always = true;
           style_user = "bold_purple";
@@ -72,6 +76,36 @@ in
         };
         nix_shell = {
           symbol = "nix ";
+        };
+        palettes = {
+          catppuccin_macchiato = {
+            rosewater = "#f4dbd6";
+            flamingo = "#f0c6c6";
+            pink = "#f5bde6";
+            mauve = "#c6a0f6";
+            red = "#ed8796";
+            maroon = "#ee99a0";
+            peach = "#f5a97f";
+            yellow = "#eed49f";
+            green = "#a6da95";
+            teal = "#8bd5ca";
+            sky = "#91d7e3";
+            sapphire = "#7dc4e4";
+            blue = "#8aadf4";
+            lavender = "#b7bdf8";
+            text = "#cad3f5";
+            subtext1 = "#b8c0e0";
+            subtext0 = "#a5adcb";
+            overlay2 = "#939ab7";
+            overlay1 = "#8087a2";
+            overlay0 = "#6e738d";
+            surface2 = "#5b6078";
+            surface1 = "#494d64";
+            surface0 = "#363a4f";
+            base = "#24273a";
+            mantle = "#1e2030";
+            crust = "#181926";
+          };
         };
       };
     };
