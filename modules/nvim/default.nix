a07c2ba12572b1c20c1160c6558087a2b925aaf9
phamann <patrick@fastly.com>
2023-05-03 10:22:42 +0100

    attempt to fix nvim lua runtime path with symlink

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -9,18 +9,14 @@ in
 {
   options.modules.nvim = { enable = mkEnableOption "nvim"; };
   config = mkIf cfg.enable {
-    home.file."lazy-lock.json".source = ./lazy-lock.json;
-    /* home.packages = with pkgs; [
-        neovim
-    ]; */
     programs.neovim = {
         enable = true;
     };
     xdg.configFile = {
       nvim = {
-      source =
-        config.lib.file.mkOutOfStoreSymlink
-          "${config.home.homeDirectory}/.config/nixpkgs/modules/nvim/config";
+        source =
+          config.lib.file.mkOutOfStoreSymlink
+            "${config.home.homeDirectory}/.config/nixpkgs/modules/nvim/config";
         recursive = true;
       };
     };
