622258a96f1c1dcce9ce0d662b3d6cbf3e6b6aaf
Patrick Hamann <patrick@fastly.com>
2023-07-09 23:04:34 +0100

    More osx configuration via nix-darwin

--- hosts/M41R603WVM/configuration.nix
+++ hosts/M41R603WVM/configuration.nix
@@ -5,5 +5,47 @@
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
-  system.defaults.finder.AppleShowAllFiles = false;
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable sudo authentication with Touch ID.
+  security.pam.enableSudoTouchIdAuth = true;
+
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleShowAllFiles = true;
+      AppleShowScrollBars = "Always";
+      # Disable saving documents to iCloud by default.
+      NSDocumentSaveNewDocumentsToCloud = false;
+    };
+
+    # Dock settings
+    dock = {
+      autohide = false;
+      showhidden = true;
+      tilesize = 50;
+
+      # Don't rearrange spaces based on their recent use.
+      mru-spaces = false;
+    };
+
+    # Finder settings
+    finder = {
+      # Show hidden files
+      AppleShowAllFiles = true;
+
+      # Don't display files/icons on desktop.
+      CreateDesktop = false;
+
+      FXEnableExtensionChangeWarning = false;
+      # Change the default finder view to column view.
+      FXPreferredViewStyle = "clmv";
+
+      QuitMenuItem = true;
+      # Show path breadcrumbs in finder windows
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+  };
 }
