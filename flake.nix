16fe1687879141e899c5cd83895f0a9dba6b2eb1
Patrick Hamann <patrick@fastly.com>
2024-01-03 12:44:21 +0000

    Bump nixpkgs release to 23.11

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Patrick Hamann's dotfile and home configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
