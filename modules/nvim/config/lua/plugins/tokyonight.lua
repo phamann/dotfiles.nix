c0e29c6ad30f77f8d8343f12003f67ccb78b2322
Patrick Hamann <patrick@fastly.com>
2024-01-10 10:31:29 +0000

    Move to neo-tree

--- modules/nvim/config/lua/plugins/tokyonight.lua
+++ modules/nvim/config/lua/plugins/tokyonight.lua
@@ -10,34 +10,36 @@ return {
             styles = {
                 -- Style to be applied to different syntax groups
                 -- Value is any valid attr-list value for `:help nvim_set_hl`
-                comments = { italic = true },
-                keywords = { italic = true },
+                comments = {italic = true},
+                keywords = {italic = true},
                 functions = {},
                 variables = {},
                 -- Background styles. Can be "dark", "transparent" or "normal"
                 sidebars = "dark", -- style for sidebars, see below
-                floats = "transparent", -- style for floating windows
+                floats = "transparent" -- style for floating windows
             },
             --[[ on_highlights = function(hl, c)
                 hl.CursorLine = { c.}
             end, ]]
             on_highlights = function(hl, c)
-              hl.CursorLineNr = { fg = c.orange, bold = true }
-              hl.LineNr = { fg = c.orange, bold = true }
-              hl.LineNrAbove = { fg = c.fg_gutter }
-              hl.LineNrBelow = { fg = c.fg_gutter }
-              local prompt = "#2d3149"
-              hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
-              hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
-              hl.TelescopePromptNormal = { bg = prompt }
-              hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
-              hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
-              hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.orange }
-              hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.orange }
-              hl.NvimTreeOpenedFile = { bg = c.bg_dark, fg = c.orange }
-            end,
+                hl.CursorLineNr = {fg = c.orange, bold = true}
+                hl.LineNr = {fg = c.orange, bold = true}
+                hl.LineNrAbove = {fg = c.fg_gutter}
+                hl.LineNrBelow = {fg = c.fg_gutter}
+                local prompt = "#2d3149"
+                hl.TelescopeNormal = {bg = c.bg_dark, fg = c.fg_dark}
+                hl.TelescopeBorder = {bg = c.bg_dark, fg = c.bg_dark}
+                hl.TelescopePromptNormal = {bg = prompt}
+                hl.TelescopePromptBorder = {bg = prompt, fg = prompt}
+                hl.TelescopePromptTitle = {bg = c.fg_gutter, fg = c.orange}
+                hl.TelescopePreviewTitle = {bg = c.bg_dark, fg = c.orange}
+                hl.TelescopeResultsTitle = {bg = c.bg_dark, fg = c.orange}
+                hl.NvimTreeOpenedFile = {bg = c.bg_dark, fg = c.orange}
+                hl.NeoTreeModified = {bg = c.bg_dark, fg = c.orange}
+                hl.NeoTreeFileNameOpened = {bg = c.bg_dark, fg = c.orange}
+            end
         })
         -- load the colorscheme here
         vim.cmd([[colorscheme tokyonight]])
-    end,
+    end
 }
--- modules/nvim/config/lua/plugins/tree.lua
+++ modules/nvim/config/lua/plugins/tree.lua
@@ -1,33 +1,118 @@
 return {
-    "nvim-tree/nvim-tree.lua",
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     dependencies = {
-        "nvim-tree/nvim-web-devicons",
+        "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim", {
+            's1n7ax/nvim-window-picker',
+            version = '2.*',
+            config = function()
+                require("window-picker").setup({
+                    hint = 'statusline-winbar',
+                    selection_chars = 'ABCDEFGHIJ',
+                    filter_rules = {
+                        include_current_win = false,
+                        autoselect_one = false,
+                        bo = {
+                            filetype = {'neo-tree', "neo-tree-popup", "notify"},
+                            buftype = {'terminal', "quickfix"}
+                        }
+                    }
+                })
+            end
+        }
     },
+    deactivate = function() vim.cmd([[Neotree close]]) end,
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+
+            default_component_configs = {
+                name = {use_git_status_colors = true},
+                git_status = {
+                    symbols = {
+                        -- Change type
+                        added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                        deleted = "✖", -- this can only be used in the git_status source
+                        renamed = '➜',
+                        deleted = '',
+                        -- Status type
+                        untracked = '?',
+                        ignored = '◌',
+                        unstaged = 'ϟ',
+                        staged = '+',
+                        conflict = ""
+                    }
+                }
+            },
+            window = {
+                mappings = {
+                    ["P"] = {
+                        "toggle_preview",
+                        config = {use_float = true, use_image_nvim = false}
+                    },
+                    ["<cr>"] = "open_with_window_picker",
+                    ["w"] = "open_with_window_picker"
+                }
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false
+                }
+            },
+            buffers = {
+                follow_current_file = {enabled = true, leave_dirs_open = true}
+            },
+            sources = {
+                "filesystem", "buffers", "git_status", "document_symbols"
+            },
+            source_selector = {winbar = true, statusline = false}
+        })
+    end,
+    keys = {
+        {
+            "<leader>;",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                    reveal = true
+                })
+            end,
+            desc = "Toggle  NeoTree"
+        }
+    }
+}
+--[[
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {"nvim-tree/nvim-web-devicons"},
     config = function()
         require("nvim-tree").setup({
             view = {
                 relativenumber = false,
                 width = 30,
                 signcolumn = "yes",
-                number = false,
+                number = false
             },
             reload_on_bufenter = false,
-            update_focused_file = {
-                enable = true,
-            },
+            update_focused_file = {enable = true},
             diagnostics = {
                 enable = true,
                 icons = {
-                  error = "",
-                  warning = "",
-                  hint = "",
-                  info = "",
-              },
+                    error = "",
+                    warning = "",
+                    hint = "",
+                    info = ""
+                }
             },
             -- open_on_setup_file = true,
-            git = {
-                enable = true,
-            },
+            git = {enable = true},
             renderer = {
                 highlight_opened_files = "all",
                 icons = {
@@ -43,19 +128,14 @@ return {
                             staged = '+',
                             unmerged = '',
                             unstaged = 'ϟ',
-                            untracked = '?',
-                        },
-                    },
-                },
-            },
-            actions = {
-                open_file = {
-                    quit_on_open = true
+                            untracked = '?'
+                        }
+                    }
                 }
-            }
+            },
+            actions = {open_file = {quit_on_open = true}},
+            source_selector = {winbar = true}
         })
     end,
-    keys = {
-        { "<leader>;", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
-    },
-}
+    keys = {{"<leader>;", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree"}}
+} ]]
