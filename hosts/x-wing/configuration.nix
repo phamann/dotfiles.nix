2f5767208bff4bf5ed129661f5ef17371ff81e36
Patrick Hamann <patrick@fastly.com>
2024-07-30 07:58:20 +0100

    Install tailscale daemon on x-wing

--- hosts/x-wing/configuration.nix
+++ hosts/x-wing/configuration.nix
@@ -87,4 +87,10 @@
   system.activationScripts.postUserActivation.text = ''
     /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
   '';
+
+  # Tailscale
+  # ervices.tailscale.enable = true;
+  environment.systemPackages = with pkgs; [
+    unstable.tailscale
+  ];
 }
