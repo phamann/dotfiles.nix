1b3beb80a999b961b5882dcbec938198f61cc6fe
Patrick Hamann <patrick@fastly.com>
2024-11-22 10:36:11 +0000

    Fix bat tokyonight theme

--- modules/bat/default.nix
+++ modules/bat/default.nix
@@ -9,32 +9,18 @@ in
       programs.bat = {
         enable = true;
         config = {
-          theme = "tokyonight";
-          # theme = "github_dark_dimmed";
+          theme = "tokyonight_night";
         };
         themes = {
           tokyonight = {
             src = pkgs.fetchFromGitHub {
               owner = "folke";
               repo = "tokyonight.nvim";
-              rev = "v2.9.0";
+              rev = "v4.10.0";
               hash = "sha256-NOKzXsY+DLNrykyy2Fr1eiSpYDiBIBNHL/7PPvbgbSo=";
             };
-            file = "extras/sublime/tokyonight_storm.tmTheme";
+            file = "extras/sublime/tokyonight_night.tmTheme";
           };
-          github-dark-dimmed = {
-            src = ./themes/github;
-            file = "github_dark_dimmed.tmTheme";
-          };
-          /* github-dark-dimmed = {
-            src = pkgs.fetchFromGitHub {
-              owner = "mauroreisvieira";
-              repo = "github-sublime-theme";
-              rev = "4070-2.6.0";
-              hash = "sha256-8flLM05vkeiRPwJLvOYUxO/qvDeypAFdntfH08BsFto=";
-            };
-            file = "GitHub%20Dimmed.sublime-theme";
-          }; */
         };
       };
     };
