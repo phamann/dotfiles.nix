ab3070e5434a71a603e6413c89501bc834d5d8fc
Patrick Hamann <patrick@fastly.com>
2024-11-22 10:36:52 +0000

    Add delta to git diff config

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,6 +1,12 @@
 { pkgs, lib, config, ... }:
 with lib; let
   cfg = config.modules.git;
+  tokyonight = pkgs.fetchFromGitHub {
+    owner = "folke";
+    repo = "tokyonight.nvim";
+    rev = "v2.9.0";
+    hash = "sha256-NOKzXsY+DLNrykyy2Fr1eiSpYDiBIBNHL/7PPvbgbSo=";
+  };
 in
 {
   options.modules.git = { enable = mkEnableOption "git"; };
@@ -10,21 +16,35 @@ in
         enable = true;
         userName = "Patrick Hamann";
         userEmail = "patrick@fastly.com";
+        includes = [
+          {
+            path = "${tokyonight}/extras/delta/tokyonight_night.gitconfig";
+          }
+        ];
+        delta = {
+          enable = true;
+          options = {
+            side-by-side = true;
+            line-numbers = true;
+            hunk-header-decoration-style = "";
+            hunk-header-style = "file line-number syntax";
+          };
+        };
         signing = {
-            key = "CD2E6283475DC528";
-            signByDefault = true;
+          key = "CD2E6283475DC528";
+          signByDefault = true;
         };
         extraConfig = {
-            init = { defaultBranch = "main"; };
-            url = {
-              "git@github.com:" = {
-                insteadOf = "https://github.com/";
-              };
+          init = { defaultBranch = "main"; };
+          url = {
+            "git@github.com:" = {
+              insteadOf = "https://github.com/";
             };
-            mergetool = {
-              prompt = false;
-              keepBackup = false;
-            };
+          };
+          mergetool = {
+            prompt = false;
+            keepBackup = false;
+          };
         };
         aliases = {
           st = "status";
