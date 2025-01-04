51b1a93d7eb760028f7a8fa080424b0e522e2137
Patrick Hamann <patrick@fastly.com>
2025-01-04 19:17:27 +0000

    Update

--- modules/ghostty/default.nix
+++ modules/ghostty/default.nix
@@ -1,17 +1,10 @@
 { pkgs, lib, config, ... }:
-with lib; let
-  cfg = config.modules.ghostty;
-in
-{
+with lib;
+let cfg = config.modules.ghostty;
+in {
   options.modules.ghostty = { enable = mkEnableOption "ghostty"; };
-  config =
-    mkIf cfg.enable {
-      programs.ghostty = {
-        enable = true;
-        enableZshIntegration = true;
-        settings = {
-          font-family = "JetBrainsMono Nerd Font Mono";
-        };
-      };
-    };
+  config = mkIf cfg.enable {
+    programs.zellij = { enable = true; };
+    home.file = { ".config/ghostty/config".source = ./config; };
+  };
 }
