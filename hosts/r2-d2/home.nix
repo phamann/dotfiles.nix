cfab3c2bb8c6c413c05e76757997cde9f966f1ee
Patrick Hamann <patrick.hamann@incident.io>
2026-05-16 17:10:01 +0100

    chore(hosts): document tested scheme alternates on r2-d2
    Active scheme unchanged (catppuccin-macchiato). Adds commented-out
    alternates as a scratchpad for future scheme exploration.

--- hosts/r2-d2/home.nix
+++ hosts/r2-d2/home.nix
@@ -1,5 +1,14 @@
 _: {
   imports = [ ../../profiles/work-laptop.nix ];
 
+  # modules.theme.scheme = "catppuccin-frappe";
   modules.theme.scheme = "catppuccin-macchiato";
+  # modules.theme.scheme = "catppuccin-mocha";
+  # modules.theme.scheme = "tokyo-night-moon";
+  # modules.theme.scheme = "tomorrow-night";
+  # # modules.theme.scheme = "chalkboard";
+  # modules.theme.scheme = "later-this-evening";
+  # modules.theme.scheme = "space-gray-eighties-dull";
+  # modules.theme.scheme = "0x96f";
+  # modules.theme.scheme = "ayu-mirage";
 }
