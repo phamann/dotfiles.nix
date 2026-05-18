e58b1947267eff2de0dcdf7a36b1fc1fe6eaf584
Patrick Hamann <patrick.hamann@incident.io>
2026-05-18 08:36:01 +0100

    chore(r2-d2): switch active scheme to base24-ocean-custom
    base24-ocean-custom is the in-repo darker variant of base24-ocean
    (see modules/theme/schemes/). Expanded commented-out alternate list
    to include a few base16 variants and additional base24 schemes
    discovered during testing.

--- hosts/r2-d2/home.nix
+++ hosts/r2-d2/home.nix
@@ -2,13 +2,20 @@ _: {
   imports = [ ../../profiles/work-laptop.nix ];
 
   # modules.theme.scheme = "base24-catppuccin-frappe";
-  modules.theme.scheme = "base24-catppuccin-macchiato";
+  # modules.theme.scheme = "base24-catppuccin-macchiato";
   # modules.theme.scheme = "base24-catppuccin-mocha";
   # modules.theme.scheme = "base24-tokyo-night-moon";
   # modules.theme.scheme = "base24-tomorrow-night";
-  # modules.theme.scheme = "base24-chalkboard";
-  # modules.theme.scheme = "base24-later-this-evening";
-  # modules.theme.scheme = "base24-space-gray-eighties-dull";
+  # modules.theme.scheme = "base24-later-this-evening"; ## *
+  # modules.theme.scheme = "base24-space-gray-eighties-dull"; ## *
   # modules.theme.scheme = "base24-0x96f";
-  # modules.theme.scheme = "base24-ayu-mirage";
+  # modules.theme.scheme = "base24-ayu-mirage"; ## *
+  # modules.theme.scheme = "base24-mission-brogue";
+  # modules.theme.scheme = "base16-ashes";
+  # modules.theme.scheme = "base16-edge-dark"; ## *
+  # modules.theme.scheme = "base16-ocean"; # # *
+  # modules.theme.scheme = "base24-one-dark";
+  # modules.theme.scheme = "base24-pnevma";
+  # modules.theme.scheme = "base24-ocean"; # # *
+  modules.theme.scheme = "base24-ocean-custom"; # # *
 }
